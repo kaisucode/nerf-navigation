0e8c2777f80272add81a3c9357d8d583ebc84b36
johnnylu305 <clu52@gpu2115.oscar.ccv.brown.edu>
2023-05-06 18:40:54 -0400

    add spot vis loader

--- NeRF/ngp_pl/datasets/__init__.py
+++ NeRF/ngp_pl/datasets/__init__.py
@@ -3,19 +3,18 @@ from .nsvf import NSVFDataset
 from .colmap import ColmapDataset
 from .nerfpp import NeRFPPDataset
 from .rtmv import RTMVDataset
-from .spot import SpotDataset
-from .brics import BRICSDataset
+#from .spot import SpotDataset
+#from .brics import BRICSDataset
+from .spot_vis import SpotVisDataset
 from .spot_online import SpotOnlineDataset
-# from .spot import SpotDataset
+
 
 dataset_dict = {'nerf': NeRFDataset,
                 'nsvf': NSVFDataset,
                 'colmap': ColmapDataset,
                 'nerfpp': NeRFPPDataset,
                 'rtmv': RTMVDataset,
-                "spot": SpotDataset,
-                "brics": BRICSDataset,
+                'spot_vis': SpotVisDataset,
+                #"spot": SpotDataset,
+                #"brics": BRICSDataset,
                 'spot_online': SpotOnlineDataset}
-
-
-                # 'spot': SpotDataset}
