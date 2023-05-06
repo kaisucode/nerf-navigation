25b77d531d008bd3e1a127c096d920444dbb1b45
johnnylu305 <a2694815@gmail.com>
2023-05-06 12:43:06 -0400

    new alignment

--- NeRF/ngp_pl/odom_to_ngp.py
+++ NeRF/ngp_pl/odom_to_ngp.py
@@ -2,9 +2,58 @@ import json
 import os
 import numpy as np
 from scipy.spatial.transform import Rotation
-from datasets.ray_utils import rotateAxis
-from utils import load_transform_json
+from utils import load_transform_json, align_odom_to_colmap
 
+
+def rotateAxis(degrees, axis):
+    '''
+    Function to rotate around given axis
+    Input:
+        degrees - scalar - Angle in degrees
+        
+        axis - scalar - options:
+            0 - around x axis
+            1 - around y axis
+            2 - around z axis  
+    
+    Returns:
+        Homogeneous rotation matrix
+    '''
+
+    radians = np.radians(degrees)
+
+    if axis == 2: # z - axis
+
+        rotation_mat = np.array([[np.cos(radians), -np.sin(radians),           0,          0],
+                                 [np.sin(radians),  np.cos(radians),           0,          0],
+                                 [              0,                0,           1,          0],
+                                 [              0,                0,           0,          1]])
+
+    elif axis == 1: # y - axis
+
+        rotation_mat = np.array([[np.cos(radians),                0,  np.sin(radians),          0],
+                                 [              0,                1,                0,          0],
+                                 [-np.sin(radians),               0, np.cos(radians),          0],
+                                 [              0,                0,                0,          1]])
+
+    elif axis == 0: # x - axis
+
+
+        rotation_mat = np.array([[             1,                0,                0,          0],
+                                [              0,  np.cos(radians), -np.sin(radians),          0],
+                                [              0,  np.sin(radians),  np.cos(radians),          0], 
+                                [              0,                0,                0,          1]])
+    
+    return rotation_mat
+
+def translateMatrix(x, y, z):
+    
+    translation_matrix = np.eye(4)
+    translation_matrix[0,3] += x
+    translation_matrix[1,3] += y
+    translation_matrix[2,3] += z
+
+    return translation_matrix
 
 def save_as_transform_json(parent, poses):
     data = load_transform_json(parent, 0, -1)
@@ -28,33 +77,42 @@ def read_camera_json_sorted(path, start = 0, end = -1):
         poses.append(T_c)
     
     poses = np.array(poses)
-    print(poses.shape)
+    #print(poses.shape)
     return poses
 
-    
-
-def get_odom_to_nerf_matrix(parent, ts, qs, scale=3):
+def get_odom_to_nerf_matrix(parent, ts, qs, scale=1):
     data = load_transform_json(parent, 0, -1)
-    cam_id = int(data['frames'][0]['file_path'].split(os.sep)[-1][:-4])
 
-    frame_change = rotateAxis(-90, 2) @ rotateAxis(-90, 0) @ rotateAxis(180, 0)
+    #frame_change = rotateAxis(-90, 2) @ rotateAxis(-90, 0) @ rotateAxis(180, 0)
     bottom = np.array([[0, 0, 0, 1.]]) 
+    
+    poses_colmap = []
+    poses_odom = []
+    for i in range(len(data['frames'])):
+        cam_id = int(data['frames'][0]['file_path'].split(os.sep)[-1][:-4])
+        poses_colmap.append(data['frames'][i]['transform_matrix'])
+        
+        # quaternion to rotation matrix
+        R = Rotation.from_quat(np.array([qs[cam_id][1], qs[cam_id][2], 
+                                         qs[cam_id][3], qs[cam_id][0]])).as_matrix()
+        # to 4 x 4 matrix
+        c2w = np.vstack([np.hstack([R, ts[cam_id][..., None]]), bottom]) #@ frame_change
+        c2w[:3, 3] *= scale
+        #c2w[:3, 3] += np.random.normal(0, 1, 3)*0.00001
 
-    # quaternion to rotation matrix
-    R = Rotation.from_quat(np.array([qs[cam_id][1], qs[cam_id][2], 
-                                     qs[cam_id][3], qs[cam_id][0]])).as_matrix()
-    # to nerf coordinate
-    c2w = np.vstack([np.hstack([R, ts[cam_id][..., None]]), bottom]) @ frame_change
-    c2w[:3, 3] *= scale
-    # find o2n
-    o2n = np.array(data['frames'][0]['transform_matrix'])[:3, :3]@np.linalg.inv(c2w[:3, :3])
-    offset = np.array(data['frames'][0]['transform_matrix'])[:3, 3]-o2n@c2w[:3, 3]
+        poses_odom.append(c2w)
 
-    return o2n, offset
+    # N x 4 x 4
+    # N x 4 x 4
+    poses_colmap = np.array(poses_colmap)
+    poses_odom = np.array(poses_odom)
 
-def odom_to_nerf(parent, ts, qs, o2n, offset, scale=3, to_ngp=False):
-    # not sure why the orientation is reversed with rotateAxis(180, 0)
-    frame_change = rotateAxis(-90, 2) @ rotateAxis(-90, 0) #@ rotateAxis(180, 0)
+    c, R, t = align_odom_to_colmap(poses_colmap[:, :3], poses_odom[:, :3])
+    #print(c, R, t)
+    return c*R, t
+
+def odom_to_nerf(parent, ts, qs, o2n, offset, scale=1, to_ngp=False):
+    #frame_change = rotateAxis(-90, 2) @ rotateAxis(-90, 0) @ rotateAxis(180, 0)
     bottom = np.array([[0, 0, 0, 1.]]) 
 
     poses = []
@@ -62,28 +120,28 @@ def odom_to_nerf(parent, ts, qs, o2n, offset, scale=3, to_ngp=False):
         # quaternion to rotation matrix
         R = Rotation.from_quat(np.array([qs[i][1], qs[i][2], qs[i][3], qs[i][0]])).as_matrix()
         # to nerf coordinate
-        P = np.vstack([np.hstack([R, ts[i][..., None]]), bottom]) @ frame_change # 4, 4     
-        P[:3, 3] *= scale
+        P = np.vstack([np.hstack([R, ts[i][..., None]]), bottom]) #@ frame_change # 4, 4     
+        #P[:3, 3] *= scale
         P[:3, 3] = o2n @ P[:3, 3]
         P[:3, 3] += offset
-        poses.append(P)
         # we rescale scene by 20 in ngp dataloader
         if to_ngp:
-            poses[:3, 3] /= 20
+            P[:3, 3] /= 20
+        poses.append(P)
     return np.array(poses)
 
 
 if __name__=="__main__":
-    parent = "../../../../robot/spot_data_/spot_data_0/"
+    parent = "/home/johnnylu/Downloads/spot_data_/spot_data_0/"
     # read T and Q from odom
     ts = np.load(os.path.join(parent, "arr_2.npy"))
     qs = np.load(os.path.join(parent, "arr_3.npy"))
     
-    o2n, offset = get_odom_to_nerf_matrix(parent, ts, qs, 3)
-    print(o2n)
-    print(offset)
+    o2n, offset = get_odom_to_nerf_matrix(parent, ts, qs, 1)
+    #print(o2n)
+    #print(offset)
 
-    poses = odom_to_nerf(parent, ts, qs, o2n, offset, scale=3, to_ngp=False)
+    poses = odom_to_nerf(parent, ts, qs, o2n, offset, scale=1, to_ngp=True)
     print(poses.shape)
 
-    save_as_transform_json(parent, poses)
+    #save_as_transform_json(parent, poses)
